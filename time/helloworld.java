877f852d5e374de4b1236740cb5e8a4e049c0194
John Wang <johncwang@gmail.com>
2023-09-03 19:42:28 -0700

    feat: `time`: add example with RFC-3339 time

---
+++ time/helloworld.java
@@ -0,0 +1,31 @@
+///usr/bin/env jbang "$0" "$@" ; exit $? 
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.ZoneOffset;
+import java.lang.Thread;
+import java.time.ZonedDateTime;
+
+class helloworld { 
+    public static void main(String[] args) {
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZZZZZ");
+        String dtStr0 = fmt.format(ZonedDateTime.now());
+        System.out.println(dtStr0);
+
+        if(args.length==0) {
+            System.out.println("Hello World!");
+        } else {
+            System.out.println("Hello " + args[0]);
+        }
+
+		try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+        String dtStr1 = fmt.format(ZonedDateTime.now());
+        System.out.println(dtStr1);
+    }
+}
