a8ced127bd92c456d2a55c25dddca5e282a4b265
John Wang <johncwang@gmail.com>
2023-09-03 19:58:54 -0700

    style: replace tabs

--- stdlib/helloworld.java
+++ stdlib/helloworld.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
 class helloworld { 
     public static void main(String[] args) {
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZZZZZ");
-		ZonedDateTime dt0 = ZonedDateTime.now();
+        ZonedDateTime dt0 = ZonedDateTime.now();
         String dtStr0 = fmt.format(dt0);
         System.out.println(dtStr0);
 
@@ -25,13 +25,13 @@ class helloworld {
             System.out.println(e);
         }
 
-		ZonedDateTime dt1 = ZonedDateTime.now();
+        ZonedDateTime dt1 = ZonedDateTime.now();
         String dtStr1 = fmt.format(dt1);
         System.out.println(dtStr1);
 
-		Duration dur = Duration.between(dt0,dt1);
-		long durms = dur.toMillis();
-		long durmn = TimeUnit.MILLISECONDS.toMinutes(durms)*60;
+        Duration dur = Duration.between(dt0,dt1);
+        long durms = dur.toMillis();
+        long durmn = TimeUnit.MILLISECONDS.toMinutes(durms)*60;
         // String time = String.format("%02d hours, %02d min, %02d sec",
         String time = String.format("%02d:%02d:%02d",
                 TimeUnit.MILLISECONDS.toHours(durms),
